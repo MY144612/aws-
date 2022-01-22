ab065169e9d1e306f805201e7bfa400fe55bbc14
Mahmoud Hisham <mahmoudhisham144612@gmail.com>
2022-01-22 14:03:42 +0200

    python file for using docker

---
+++ file9.py
@@ -0,0 +1,18 @@
+l= []
+n=int(input("entre the number of elements : "))
+i=0
+while(i<n):
+ v=int(input())
+ l.append( v)
+ i+=1
+
+x=input(" A or D or N  ")
+if (x == "A"):
+  l.sort()
+  print(l)
+elif (x == "D"):
+ l.sort()
+ l.reverse()
+ print(l)
+else:
+ print(l)
